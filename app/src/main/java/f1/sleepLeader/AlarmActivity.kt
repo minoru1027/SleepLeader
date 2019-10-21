a34a86c5247b3476c9d1e63db89589b4855579d3
Keita Aiura <1701121@st.asojuku.ac.jp>
2019-10-21 09:49:14 +0900

    add:わからないもの

--- app/src/main/java/f1/sleepLeader/AlarmActivity.kt
+++ app/src/main/java/f1/sleepLeader/AlarmActivity.kt
@@ -1,13 +1,19 @@
 package f1.sleepLeader
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class AlarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        aaaaaaaaaaaaa
+
+            AlarmSet.setOnClickListener {
+                val intent = Intent(applicationContext, AlarmSetActivity::class.java)
+                startActivity(intent)
+            }
     }
 }
