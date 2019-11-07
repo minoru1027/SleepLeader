0b7fe4b93eb3d4066c3cfb9a6bf1d95b78f3b46b
minoru1027 <1701126@st.asojuku.ac.jp>
2019-11-07 11:32:51 +0900

    fix:AlarmSet修正

--- app/src/main/java/f1/sleepLeader/AlarmSetActivity.kt
+++ app/src/main/java/f1/sleepLeader/AlarmSetActivity.kt
@@ -5,15 +5,20 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.icu.util.Calendar
 //import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
 import io.realm.kotlin.createObject
 import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_alarm_set.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import java.lang.IllegalArgumentException
+import java.lang.invoke.MutableCallSite
+import java.lang.reflect.Array.get
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -22,6 +27,8 @@ import java.util.*
 class AlarmSetActivity : AppCompatActivity() {
 
     private lateinit var realm : Realm
+    private var musicPath : String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,14 +36,42 @@ class AlarmSetActivity : AppCompatActivity() {
 
         realm = Realm.getDefaultInstance()
 
+
+        val musicList = realm.where<MusicTable>().findAll()
+        musicSpinner.adapter = musicAdapter(musicList)
+        musicSpinner.visibility = View.GONE
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        musicFlag.setOnClickListener{
+            if(musicFlag.isChecked === true) {
+                musicSpinner.visibility = View.VISIBLE
+            }else{
+                musicSpinner.visibility = View.GONE
+            }
+        }
+
+        musicSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                val musicPosition = musicSpinner.getItemAtPosition(position) as MusicTable
+                musicPath = musicPosition.musicPath
+                println(musicPath)
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
         AlarmSetting.setOnClickListener {
             var calendar: Calendar = Calendar.getInstance()
             realm.executeTransaction {
                 var maxId = realm.where<AlarmTable>().max("alarmId")
                 var nextId = (maxId?.toLong() ?: 0L) + 1
                 var alarm = realm.createObject<AlarmTable>(nextId)
-
-                println(alarm.alarmId)
 
                 //設定した時間
                 var h = SetTime.text.toString()
@@ -45,47 +80,39 @@ class AlarmSetActivity : AppCompatActivity() {
                 var min = Integer.parseInt(m)
                 var time = "%1$02d:%2$02d".format(hour,min)
                 alarm.timer = time
-                println(alarm.timer)
 
                 //スヌーズ設定
-                var snoozeFlag = "ON"
-                alarm.snoozeFlag = snoozeFlag
+                var snooze = snoozeFlag.isChecked.toString()
 
-                println(alarm.snoozeFlag)
+                alarm.snoozeFlag = snooze
 
                 //音楽の再生設定
-                var musicFlag = "ON"
-                alarm.musicFlag = musicFlag
-
-                println(alarm.musicFlag)
+                var music = musicFlag.isChecked.toString()
+                alarm.musicFlag = music
 
                 //音楽のファイルパス
-                var musicPath = "www"
                 alarm.musicPath = musicPath
-
-                println(alarm.musicPath)
+                System.out.println(alarm.musicPath)
             }
-                var sec = setSecond()
+            var sec = setSecond()
 
-                calendar.add(Calendar.SECOND, sec)
+            calendar.add(Calendar.SECOND, sec)
 
-                val alarmIntent = Intent(this, AlarmReceiver::class.java)
-                val pendingIntent = PendingIntent.getBroadcast(
-                    this,
-                    0,
-                    alarmIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT
-                )
+            val alarmIntent = Intent(this, AlarmReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                alarmIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
-                val manager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                manager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
+            val manager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            manager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
 
-                val intent = Intent(applicationContext, AlarmStopActivity::class.java)
-                startActivity(intent)
+            val intent = Intent(applicationContext, AlarmStopActivity::class.java)
+            startActivity(intent)
         }
-
     }
-
     private fun setHour(): Int {
 
         val calender: Calendar = Calendar.getInstance()
