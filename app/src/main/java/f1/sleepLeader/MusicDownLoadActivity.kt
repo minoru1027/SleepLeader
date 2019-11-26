b5afde75752b51b76b8440b87a8abb8ec9120983
minoru1027 <1701126@st.asojuku.ac.jp>
2019-11-26 10:44:43 +0900

    add:firebase

---
+++ app/src/main/java/f1/sleepLeader/MusicDownLoadActivity.kt
@@ -0,0 +1,15 @@
+package f1.sleepLeader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+class MusicDownLoadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_down_load)
+
+    }
+}
